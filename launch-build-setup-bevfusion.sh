f857d02927e852bdc14dffd1876fa6d83a7947f9
Tony Ubuntu Native <antonistragoudaras@gmail.com>
2023-03-15 01:42:03 +0300

    build setup for bevfusion and data prep. nuscens

---
+++ launch-build-setup-bevfusion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# entire script fails if a single command fails
+set -e
+
+# script should be run from the project directory
+PROJECT_DIR="$PWD"
+
+# project should have a src directory
+SRC_DIR="$PROJECT_DIR"
+
+JOB_NAME=build-setup-bevfusion
+
+sbatch --job-name "$JOB_NAME" "$SRC_DIR"/build-setup-bevfusion.sbatch
