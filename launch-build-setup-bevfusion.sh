218ff33d641494b280d1373e6ebc4c428fd4249a
Tony Ubuntu Native <antonistragoudaras@gmail.com>
2023-03-21 19:36:03 +0300

    updated job name for setup of bevfusion with A100

--- launch-build-setup-bevfusion.sh
+++ launch-build-setup-bevfusion.sh
@@ -9,6 +9,6 @@ PROJECT_DIR="$PWD"
 # project should have a src directory
 SRC_DIR="$PROJECT_DIR"
 
-JOB_NAME=build-setup-bevfusion-A100
+JOB_NAME=build-setup-bevfusion-A100-capability
 
 sbatch --job-name "$JOB_NAME" "$SRC_DIR"/build-setup-bevfusion.sbatch
