13e55f20b2812820cef7aa62dae927c02f756024
Tony Ubuntu Native <antonistragoudaras@gmail.com>
2023-03-21 20:19:53 +0300

    inference using A100

--- launch-inference-nuscense-mini-det.sh
+++ launch-inference-nuscense-mini-det.sh
@@ -12,7 +12,6 @@ JOB_RESULTS_DIR="$PROJECT_DIR"/inference-nuscenses-mini-lidar+camera-logs
 
 mkdir -p "$JOB_RESULTS_DIR"
 
-
 JOB_NAME=inference-nuscenes-mini-lidar+camera
 
 sbatch --job-name "$JOB_NAME" "$SRC_DIR"/inference-nuscense-mini-det.sbatch
