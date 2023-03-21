707d3de4aba1d11411a34fd845e951f6c013af95
Tony Ubuntu Native <antonistragoudaras@gmail.com>
2023-03-21 18:05:30 +0300

    build bevfusion with a100 compute capabilities

--- launch-build-setup-bevfusion.sh
+++ launch-build-setup-bevfusion.sh
@@ -9,6 +9,6 @@ PROJECT_DIR="$PWD"
 # project should have a src directory
 SRC_DIR="$PROJECT_DIR"
 
-JOB_NAME=build-setup-bevfusion
+JOB_NAME=build-setup-bevfusion-A100
 
 sbatch --job-name "$JOB_NAME" "$SRC_DIR"/build-setup-bevfusion.sbatch
