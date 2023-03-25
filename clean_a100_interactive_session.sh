5276449902a29958a88a70f2100ffc1cc08ceb94
Tony Ubuntu Native <antonistragoudaras@gmail.com>
2023-03-26 01:53:30 +0300

    clean script after interactive session

---
+++ clean_a100_interactive_session.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# entire script fails if a single command fails
+set -e
+
+# script should be run from the project directory
+PROJECT_DIR="$PWD"
+
+rm -rf PROJECT_DIR/*interactive-a100-session*
+rm -rf PROJECT_DIR/2023*
