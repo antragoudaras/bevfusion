420fcb9ee636ca9868ae758909821506025ace6a
Tony Ubuntu Native <antonistragoudaras@gmail.com>
2023-03-26 01:55:35 +0300

    update script

--- clean_a100_interactive_session.sh
+++ clean_a100_interactive_session.sh
@@ -6,5 +6,5 @@ set -e
 # script should be run from the project directory
 PROJECT_DIR="$PWD"
 
-rm -rf PROJECT_DIR/*interactive-a100-session*
+rm -rf PROJECT_DIR/interactive-a100-session*
 rm -rf PROJECT_DIR/2023*
